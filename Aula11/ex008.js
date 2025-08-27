0f7ab22ae41863660221260297e0d94114e4c54e
RonyNjr <122393881+RonyNjr@users.noreply.github.com>
2025-08-27 15:11:35 -0300

    Att-ex009test

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`a velocidade do seu carro e ${vel}Km/h`)
+if (vel > 60) { // condicao simples
+    console.log(`voce ultrapassou o limite de velocidade. MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de seguranca`)
